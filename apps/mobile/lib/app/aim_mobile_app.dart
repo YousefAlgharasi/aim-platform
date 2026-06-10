3cc080584ca784471a8ac92bdfa50b9722ba7c97
Yousef Algharasi <yousefalgharasi2@gmail.com>
2026-06-11 02:05:37 +0300

    P1-038: create Flutter Mobile project shell"

---
+++ apps/mobile/lib/app/aim_mobile_app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AimMobileApp extends StatelessWidget {
+  const AimMobileApp({super.key});
+
+  static const String appTitle = 'AIM Mobile';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const AimShellHomePage(),
+    );
+  }
+}
+
+class AimShellHomePage extends StatelessWidget {
+  const AimShellHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AimMobileApp.appTitle),
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Text(
+            'AIM Flutter Mobile shell is ready.',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ apps/mobile/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/aim_mobile_app.dart';
+
+void main() {
+  runApp(const AimMobileApp());
+}
