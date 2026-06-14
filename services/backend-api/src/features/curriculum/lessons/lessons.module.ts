ccc4c2d0dc8368689c6c7fb0dfb4768d187da2ef
GHOST3030 <ahmedalalawi41@gmail.com>
2026-06-14 16:20:35 +0300

    refactor(modules): add DatabaseModule and user-related modules to lesson and session modules

--- services/backend-api/src/features/curriculum/lessons/lessons.module.ts
+++ services/backend-api/src/features/curriculum/lessons/lessons.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from '../../../auth/auth.module';
+import { DatabaseModule } from '../../../database/database.module';
+import { RolesModule } from '../../roles/roles.module';
+import { UsersModule } from '../../users/users.module';
 import { LessonsController } from './lessons.controller';
 import { LessonsService } from './lessons.service';
 
 @Module({
+  imports: [AuthModule, DatabaseModule, RolesModule, UsersModule],
   controllers: [LessonsController],
   providers: [LessonsService],
   exports: [LessonsService],
--- services/backend-api/src/features/lessons/lessons.module.ts
+++ services/backend-api/src/features/lessons/lessons.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { DatabaseModule } from '../../database/database.module';
 import { LessonsService } from './lessons.service';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [LessonsService],
   exports: [LessonsService],
 })
