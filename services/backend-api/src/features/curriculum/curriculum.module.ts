53b2ac76e955f36f3f1046b48b083ccc47b0cd73
yo0sf <yusifakram2005@gmail.com>
2026-06-14 19:42:21 +0300

    P3-035: implement objectives service and api

--- services/backend-api/src/features/curriculum/curriculum.module.ts
+++ services/backend-api/src/features/curriculum/curriculum.module.ts
@@ -3,8 +3,10 @@ import { ChaptersModule } from './chapters/chapters.module';
 import { CoursesModule } from './courses/courses.module';
 import { LessonAssetsModule } from './lesson-assets/lesson-assets.module';
 import { LessonObjectivesModule } from './lesson-objectives/lesson-objectives.module';
-import { LevelsModule } from './levels/levels.module';
+import { LessonSkillsModule } from './lesson-skills/lesson-skills.module';
 import { LessonsModule } from './lessons/lessons.module';
+import { LevelsModule } from './levels/levels.module';
+import { ObjectivesModule } from './objectives/objectives.module';
 import { QuestionBankModule } from './question-bank/question-bank.module';
 import { SkillsModule } from './skills/skills.module';
 
@@ -13,17 +15,25 @@ import { SkillsModule } from './skills/skills.module';
     CoursesModule,
     LevelsModule,
     ChaptersModule,
-    SkillsModule,
+    LessonsModule,
+    LessonSkillsModule,
     LessonAssetsModule,
     LessonObjectivesModule,
+    ObjectivesModule,
+    SkillsModule,
+    QuestionBankModule,
   ],
   exports: [
     CoursesModule,
     LevelsModule,
     ChaptersModule,
-    SkillsModule,
+    LessonsModule,
+    LessonSkillsModule,
     LessonAssetsModule,
     LessonObjectivesModule,
+    ObjectivesModule,
+    SkillsModule,
+    QuestionBankModule,
   ],
 })
 export class CurriculumModule {}
