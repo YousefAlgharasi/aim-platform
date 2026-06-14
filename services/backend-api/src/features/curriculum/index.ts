7a1850a80ad5bf5b663666938bfc475085d362ed
GHOST3030 <157970804+GHOST3030@users.noreply.github.com>
2026-06-14 11:36:15 +0000

    P3-034: implement skills service and API

--- services/backend-api/src/features/curriculum/index.ts
+++ services/backend-api/src/features/curriculum/index.ts
@@ -2,3 +2,5 @@ export { CurriculumModule } from './curriculum.module';
 export { CurriculumPermission } from './curriculum.permissions';
 export { CoursesService } from './courses/courses.service';
 export { LevelsService } from './levels/levels.service';
+export { ChaptersService } from './chapters/chapters.service';
+export { SkillsService } from './skills/skills.service';
