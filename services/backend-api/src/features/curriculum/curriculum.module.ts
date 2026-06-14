4859907408d67818c76d4bc163275f2a88fd1224
AIM Agent <yousefalgharasi2@gmail.com>
2026-06-14 11:55:55 +0000

    P3-037: implement lessons service and API

--- services/backend-api/src/features/curriculum/curriculum.module.ts
+++ services/backend-api/src/features/curriculum/curriculum.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChaptersModule } from './chapters/chapters.module';
 import { CoursesModule } from './courses/courses.module';
 import { LevelsModule } from './levels/levels.module';
+import { LessonsModule } from './lessons/lessons.module';
 import { SkillsModule } from './skills/skills.module';
 
 @Module({
-  imports: [CoursesModule, LevelsModule, ChaptersModule, SkillsModule],
-  exports: [CoursesModule, LevelsModule, ChaptersModule, SkillsModule],
+  imports: [CoursesModule, LevelsModule, ChaptersModule, LessonsModule, SkillsModule],
+  exports: [CoursesModule, LevelsModule, ChaptersModule, LessonsModule, SkillsModule],
 })
 export class CurriculumModule {}
