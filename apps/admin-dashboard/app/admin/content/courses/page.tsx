e841e9e6148200bc0134429bf344a35908079369
GHOST3030 <ahmedalalawi41@gmail.com>
2026-06-14 17:18:57 +0300

    P3-051: build admin curriculum navigation UI

---
+++ apps/admin-dashboard/app/admin/content/courses/page.tsx
@@ -0,0 +1,6 @@
+import { AdminCurriculumPlaceholderPage } from '../../../../components/admin-curriculum-placeholder-page';
+import { adminCurriculumNavigationItems } from '../../../../lib/admin-curriculum-navigation';
+
+export default function AdminCoursesContentPage() {
+  return <AdminCurriculumPlaceholderPage item={adminCurriculumNavigationItems[0]} />;
+}
