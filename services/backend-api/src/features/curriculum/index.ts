6bad7277047c8078b29a457776a37f6c39f82efb
GHOST3030 <157970804+GHOST3030@users.noreply.github.com>
2026-06-14 11:21:16 +0000

    P3-032: implement levels service and API

--- services/backend-api/src/features/curriculum/index.ts
+++ services/backend-api/src/features/curriculum/index.ts
@@ -1,3 +1,4 @@
 export { CurriculumModule } from './curriculum.module';
 export { CurriculumPermission } from './curriculum.permissions';
 export { CoursesService } from './courses/courses.service';
+export { LevelsService } from './levels/levels.service';
