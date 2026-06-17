3b39a8063e6870736fa392125407e2c99dfabe6b
GHOST3030 <ahmedalalawi41@gmail.com>
2026-06-17 18:17:59 +0300

    Replace DSPreviewPage with AimMobileApp in main entry point

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aim_mobile/features/design_system_preview/ui/pages/ds_preview_page.dart';
+import 'package:aim_mobile/app/aim_mobile_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,7 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
   runApp(
     const ProviderScope(
-      child: DSPreviewPage(),
+      child: AimMobileApp(),
       // child: AimMobileApp(),
     ),
   );
