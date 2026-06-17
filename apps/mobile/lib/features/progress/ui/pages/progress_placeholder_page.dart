269c7edc654b032a322ac682d83ee381efca0c2d
Yousef Algharasi <yousefalgharasi2@gmail.com>
2026-06-17 22:01:00 +0000

    P6-028: adopt AIMTopAppBar in progress_placeholder_page

--- apps/mobile/lib/features/progress/ui/pages/progress_placeholder_page.dart
+++ apps/mobile/lib/features/progress/ui/pages/progress_placeholder_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/widgets/widgets.dart';
 import '../../../shell/ui/widgets/main_shell_placeholder_card.dart';
 
 class ProgressPlaceholderPage extends StatelessWidget {
@@ -7,24 +8,13 @@ class ProgressPlaceholderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: _ProgressPlaceholderAppBar(),
-      body: MainShellPlaceholderCard(
+    return Scaffold(
+      appBar: AIMTopAppBar(title: 'Progress'),
+      body: const MainShellPlaceholderCard(
         title: 'Progress',
-        description: 'Progress placeholder. Mastery and level are never calculated in Flutter.',
+        description:
+            'Progress placeholder. Mastery and level are never calculated in Flutter.',
       ),
     );
   }
-}
-
-class _ProgressPlaceholderAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const _ProgressPlaceholderAppBar();
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(title: const Text('AIM Progress'));
-  }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
