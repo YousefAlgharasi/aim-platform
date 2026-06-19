8cf037f468ca6bf79971be0b4bf24385bac01efc
GHOST3030 <ahmedalalawi41@gmail.com>
2026-06-19 15:49:12 +0300

    Fix: Require repository parameter in AuthFlowNotifier constructor

--- apps/mobile/lib/features/auth/logic/provider/auth_flow_notifier.dart
+++ apps/mobile/lib/features/auth/logic/provider/auth_flow_notifier.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../entity/auth_flow_state.dart';
 
 class AuthFlowNotifier extends StateNotifier<AuthFlowState> {
-  AuthFlowNotifier() : super(const AuthFlowState.checking());
+  AuthFlowNotifier({required Object repository}) : super(const AuthFlowState.checking());
 
   void completeBootstrap() {
     state = const AuthFlowState.signedOut();
