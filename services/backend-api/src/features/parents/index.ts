b3e5b5284d7a5f007deabcb912314cbeb773b48c
GHOST3030 <ahmedalalawi41@gmail.com>
2026-06-20 15:12:48 +0000

    P12-022: Export ParentConsentService from parents barrel

--- services/backend-api/src/features/parents/index.ts
+++ services/backend-api/src/features/parents/index.ts
@@ -3,4 +3,5 @@ export { ParentsService } from './parents.service';
 export { ParentRepository } from './parent.repository';
 export * from './parent-repository.types';
 export { ParentChildLinkService } from './parent-child-link.service';
+export { ParentConsentService } from './parent-consent.service';
 export * from './dto';
