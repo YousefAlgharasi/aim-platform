69b3e41f4bebb7133be2b0bc42a5e5ebe0d83f01
GHOST3030 <ahmedalalawi41@gmail.com>
2026-06-20 15:16:18 +0000

    P12-023: Register ParentAccessPolicyService in ParentsModule

--- services/backend-api/src/features/parents/parents.module.ts
+++ services/backend-api/src/features/parents/parents.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../auth';
 import { DatabaseModule } from '../../database/database.module';
+import { ParentAccessPolicyService } from './parent-access-policy.service';
 import { ParentChildLinkService } from './parent-child-link.service';
 import { ParentConsentService } from './parent-consent.service';
 import { ParentRepository } from './parent.repository';
@@ -10,7 +11,19 @@ import { ParentsService } from './parents.service';
 @Module({
   imports: [AuthModule, DatabaseModule],
   controllers: [ParentsController],
-  providers: [ParentsService, ParentRepository, ParentChildLinkService, ParentConsentService],
-  exports: [ParentsService, ParentRepository, ParentChildLinkService, ParentConsentService],
+  providers: [
+    ParentsService,
+    ParentRepository,
+    ParentChildLinkService,
+    ParentConsentService,
+    ParentAccessPolicyService,
+  ],
+  exports: [
+    ParentsService,
+    ParentRepository,
+    ParentChildLinkService,
+    ParentConsentService,
+    ParentAccessPolicyService,
+  ],
 })
 export class ParentsModule {}
