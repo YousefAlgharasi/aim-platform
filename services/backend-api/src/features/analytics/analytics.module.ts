8e8fa61ad3ea53445306fdaee937d52a7e77321e
Yousef Algharasi <yousefalgharasi2@gmail.com>
2026-06-21 00:40:26 +0000

    P15-029: Create Report Definition Service

--- services/backend-api/src/features/analytics/analytics.module.ts
+++ services/backend-api/src/features/analytics/analytics.module.ts
@@ -5,6 +5,7 @@ import { AnalyticsRepository } from './analytics.repository';
 import { AnalyticsEventIngestionService } from './analytics-event-ingestion.service';
 import { MetricDefinitionService } from './metric-definition.service';
 import { MetricAggregationService } from './metric-aggregation.service';
+import { ReportDefinitionService } from './report-definition.service';
 
 @Module({
   imports: [DatabaseModule],
@@ -13,12 +14,14 @@ import { MetricAggregationService } from './metric-aggregation.service';
     AnalyticsEventIngestionService,
     MetricDefinitionService,
     MetricAggregationService,
+    ReportDefinitionService,
   ],
   exports: [
     AnalyticsRepository,
     AnalyticsEventIngestionService,
     MetricDefinitionService,
     MetricAggregationService,
+    ReportDefinitionService,
   ],
 })
 export class AnalyticsModule {}
