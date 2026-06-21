98ccedb09c92d63eb5534c8ef70bab54437e18ce
Yousef Algharasi <yousefalgharasi2@gmail.com>
2026-06-21 00:39:17 +0000

    P15-026: Create Analytics Event Ingestion Service

--- services/backend-api/src/features/analytics/analytics.module.ts
+++ services/backend-api/src/features/analytics/analytics.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '../../database/database.module';
 import { AnalyticsRepository } from './analytics.repository';
+import { AnalyticsEventIngestionService } from './analytics-event-ingestion.service';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [AnalyticsRepository],
-  exports: [AnalyticsRepository],
+  providers: [AnalyticsRepository, AnalyticsEventIngestionService],
+  exports: [AnalyticsRepository, AnalyticsEventIngestionService],
 })
 export class AnalyticsModule {}
