7d68953ec745a77922b92c1a9b5a3afffcc07f86
GHOST3030 <ahmedalalawi2022@gmail.com>
2026-06-22 16:01:26 +0000

    chore: rename Android package to com.aim.ailearn
    - Update namespace and applicationId in build.gradle.kts
    - Move MainActivity.kt to com/aim/ailearn/ package dir
    - Update package declaration in MainActivity.kt

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.aim_mobile"
+    namespace = "com.aim.ailearn"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.aim_mobile"
+        applicationId = "com.aim.ailearn"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
